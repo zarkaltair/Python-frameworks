8eaf512ea9dee48142d5742577c3bc1b6f7cd0cf
zarkaltair <zarkaltair@gmail.com>
2019-01-21 21:55:22 +0500

    Try parser
    Parser on pandas

--- Pandas/test parser.py
+++ Pandas/test parser.py
@@ -1,24 +1,42 @@
 # import pandas as pd
 
-# tables = pd.read_html("https://ethgasstation.info/")
 
+# tables = pd.read_html("https://ethgasstation.info/")
 # print(tables[0])
 
 
+
+# import pandas as pd
+
+
+# pd.options.display.max_rows = 200
+# pd.options.display.max_columns = 20
+# pd.options.display.expand_frame_repr = False
+
+
+# calls_df = pd.read_html("https://www.fnb.co.za/Controller?nav=rates.forex.list.ForexRatesList",) # header={'User-Agent': 'Mozilla/5.0'})
+# print(calls_df)
+
+
+
 import pandas as pd
+
+
 pd.options.display.max_rows = 200
 pd.options.display.max_columns = 20
 pd.options.display.expand_frame_repr = False
 
 
-calls_df = pd.read_html("https://www.fnb.co.za/Controller?nav=rates.forex.list.ForexRatesList",) # header={'User-Agent': 'Mozilla/5.0'})
+calls_df = pd.read_html("https://free-proxy-list.net/")
 print(calls_df)
 
 
+
 # import pandas as pd
+
+
 # def get_forex_buy_quote(currency_code: str = 'EUR', source: str = 'FNB', order_type: str = 'buy'):
 #     """Get latest forex from FNB website
-
 #     """
 #     if source == 'FNB':
 #         tables = pd.read_html(
