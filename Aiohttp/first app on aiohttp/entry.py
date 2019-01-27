219569fc27ae67e18159c27070b6df084ac59469
Zark <zarkaltair@gmail.com>
2019-01-27 13:20:14 +0500

    add uvloop and reloader in project

--- Aiohttp/first app on aiohttp/entry.py
+++ Aiohttp/first app on aiohttp/entry.py
@@ -1,10 +1,31 @@
+import argparse
 import aiohttp
+import asyncio
 
 from main import create_app
 
+
+try:
+	import uvloop
+	asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+except ImportError:
+	print('Library uvloop is not available')
+
+
+parser = argparse.ArgumentParser(description='Demo project')
+parser.add_argument('--host', help='Host to listen', default='0.0.0.0')
+parser.add_argument('--port', help='Port to accept connections', default='5000')
+parser.add_argument('--reload', action='store_true', help='Autoreload code on change')
+
+args = parser.parse_args()
+
+if args.reload:
+	print('Start with code reload')
+	import aioreloader
+	aioreloader.start()
 
 app = create_app()
 
 
 if __name__ == '__main__':
-	aiohttp.web.run_app(app, )
+	aiohttp.web.run_app(app, host=args.host, port=args.port)
