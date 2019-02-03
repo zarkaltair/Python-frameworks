df424631365d4e4034f662ba5ac4972027751eb8
Zark <zarkaltair@gmail.com>
2019-02-04 00:22:02 +0500

    migration, tags, many-to-many

--- Flask/blog_flask/models.py
+++ Flask/blog_flask/models.py
@@ -8,6 +8,12 @@ def slugify(s):
     return re.sub(pattern, '-', s)
 
 
+post_tags = db.Table('post_tags',
+                     db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+                     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+    )
+
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(140))
@@ -21,6 +27,9 @@ class Post(db.Model):
         self.generate_slug()
 
 
+    tags = db.relationship('Tag', secondary=post_tags, backref=db.backref('posts', lazy='dynamic'))
+
+
     def generate_slug(self):
         if self.title:
             self.slug = slugify(self.title)
@@ -28,3 +37,18 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post id {}, title: {}>'.format(self.id, self.title)
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    slug = db.Column(db.String(100))
+
+
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
+
+
+    def __repr__(self):
+        return '<Tag id: {}, name: {}>'.format(self.id, self.name)
