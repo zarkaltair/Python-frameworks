57a545cffb6e0319da05ff55b0b26594683dfa16
Zark <zarkaltair@gmail.com>
2019-02-04 14:22:30 +0500

    work with db mysql, create post form

--- Flask/blog_flask/posts/blueprint.py
+++ Flask/blog_flask/posts/blueprint.py
@@ -1,14 +1,50 @@
 from flask import Blueprint
 from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
 
 from models import Post, Tag
+from .forms import PostForm
+from app import db
 
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
+
+@posts.route('/create', methods=['POST', 'GET'])
+def create_post():
+    # request for method POST
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+
+        # Check title != None
+        if title:
+            try:
+                post = Post(title=title, body=body)
+                db.session.add(post)
+                db.session.commit()
+            except:
+                print('Something wrong')
+        
+        # redirect to index page
+        return redirect(url_for('posts.index'))
+
+    # request for method GET
+    form = PostForm()
+    return render_template('posts/create_post.html', form=form)
+
+
 @posts.route('/')
 def index():
-    posts = Post.query.all()
+    query = request.args.get('query')
+    
+    if query:
+        posts = Post.query.filter(Post.title.contains(query) | Post.body.contains(query)).all()
+    else:
+        posts = Post.query.order_by(Post.created.desc())
+
     return render_template('posts/index.html', posts=posts)
 
 
