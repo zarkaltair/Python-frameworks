3ddb36138de5e18bf23b940d7630f039daee2c1d
zarkaltair <zarkaltair@gmail.com>
2019-10-14 19:19:29 +0500

    start learn asyncpg

---
+++ AsyncIO/Asyncpg/first_start.py
@@ -0,0 +1,11 @@
+import asyncio
+import asyncpg
+
+async def run():
+    conn = await asyncpg.connect(user='user', password='password',
+                                 database='database', host='127.0.0.1')
+    values = await conn.fetch('''SELECT * FROM mytable''')
+    await conn.close()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run())
